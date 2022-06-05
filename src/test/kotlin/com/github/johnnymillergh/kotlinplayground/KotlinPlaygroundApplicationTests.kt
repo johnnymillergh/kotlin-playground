0e65ecfb9b66e5b162311c09f1e5e78a22dc12f5
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2022-06-06 06:56:28 +0800

    build: initialize project
    git@github.com:johnnymillergh/kotlin-playground.git

---
+++ src/test/kotlin/com/github/johnnymillergh/kotlinplayground/KotlinPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.johnnymillergh.kotlinplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
